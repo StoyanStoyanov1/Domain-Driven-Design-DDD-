ce585db7684d77e234152ab19a25dd968205de2b
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-09-08 16:16:28 +0200

    feat: Implement Profile aggregate with properties and initial structure

---
+++ e-commerce/domain/profile/aggregates/Profile.ts
@@ -0,0 +1,30 @@
+class Profile {
+    private readonly id: string;
+    private readonly name: string;
+    private readonly email: string;
+    private readonly phoneNumber: string;
+    private readonly username: string;
+    private readonly dateOfBirth: Date;
+    private readonly address: string;
+    private readonly createdAt: Date;
+
+    readonly updatedAt: Date;
+
+    
+}
+
+
+// Id ( UUID )
+// First name
+// Middle Name ( Optional )
+// Last Name
+// Phone number
+// Username ( Optional )
+// Avatar
+// Date of birth
+// Gender
+// AuditLog History
+// CreateAt
+// UpdateAt
+// CreatedBy: UserId
+// UpdatedBy: UserId
