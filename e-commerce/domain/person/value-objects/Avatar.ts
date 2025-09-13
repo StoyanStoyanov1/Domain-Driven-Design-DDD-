ba7cd403b0ac5fbdb2296b54ebbc3e1d51af5aa3
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-09-13 23:22:25 +0200

    feat: Refactor Avatar value object to use constants for error messages and allowed formats

--- e-commerce/domain/person/value-objects/Avatar.ts
+++ e-commerce/domain/person/value-objects/Avatar.ts
@@ -3,20 +3,25 @@ import { Result } from "../../shared/core";
 import { AvatarFormatExtensions, AvatarFormatExtensionType} from "../types/";
 
 export class Avatar extends ValueObject<string> {
+    static readonly INVALID_FORMAT_ERROR = 'Invalid avatar format.';
+    static readonly CANT_BE_EMPTY = 'Avatar cannot be empty';
+    static readonly ALLOWED_FORMATS = Object.keys(AvatarFormatExtensions).join(", ");
+
     protected validate(value: string): void {
         if (!value || value.trim() === "") {
-            throw new Error("Avatar cannot be empty");
+            throw new Error(Avatar.CANT_BE_EMPTY
+            );
         }
 
         const rawExt = value.split(".").pop()?.toLowerCase();
         if (!rawExt) {
-            throw new Error("Avatar must have an extension");
+            throw new Error(Avatar.INVALID_FORMAT_ERROR);
         }
 
         const normalizedExt = AvatarFormatExtensions[rawExt as keyof typeof AvatarFormatExtensionType];
         if (!normalizedExt) {
             throw new Error(
-                `Invalid avatar format. Allowed: ${Object.keys(AvatarFormatExtensions).join(", ")}`
+                `${Avatar.INVALID_FORMAT_ERROR} Allowed: ${Avatar.ALLOWED_FORMATS}`
             );
         }
     }
